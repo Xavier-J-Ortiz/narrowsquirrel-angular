a372c028d11abc30b8397c96a772400b04ca9a1f
xortiz <xavier.ortiz.ch@gmail.com>
2017-12-10 22:44:38 -0500

    added a text box that two way modifies the title of the book. Might repurpose this as a search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { BooksComponent } from './books/books.component';
     BooksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
