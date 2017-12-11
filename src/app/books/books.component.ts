3b67066d05b395383f5c99395a1b4c692bb6b699
xortiz <xavier.ortiz.ch@gmail.com>
2017-12-10 21:58:44 -0500

    added some information and displaying details of a book. Understanding how this all connects together

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Book} from '../book';
 
 @Component({
   selector: 'app-books',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
+
+  book: Book = {
+    id: 1,
+    title: 'Hansel and Gretel'
+  };
 
   constructor() { }
 
